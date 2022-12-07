9786b72d8bfa10f284ed12e2048bcdec50548f31
tabuguia <tabuguiafrank@gmail.com>
2022-12-07 14:36:04 +0100

    connexion avec le backend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from "./home";
+import Login from './pages/Login';
+import Register from './pages/Register';
+import axios from "axios"
+import { createBrowserRouter, RouterProvider, Route, createRoutesFromElements } from "react-router-dom"
+
+import { createContext, useState } from 'react';
+
+let apiUrl = "http://localhost:8000"
+export const axiosContext = createContext({ apiUrl, axios })
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
+      <Route path='salut' element={<Home />} >
+        <Route path='a' element={<div>une url a dans home</div>} />
+      </Route>
+    </>
+  )
+)
 
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <axiosContext.Provider value={{ apiUrl, axios }}>
+        <RouterProvider router={router} />
+      </axiosContext.Provider>
     </div>
   );
 }
